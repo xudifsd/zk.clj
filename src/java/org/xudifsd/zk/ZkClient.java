c3dfec0dc22c4706e05b47d119f2a73234a41f24
Carl Xu <xudifsd@gmail.com>
2013-08-07 18:38:22 +0800

    throws Exception instead of catch it in lib

--- src/java/org/xudifsd/zk/ZkClient.java
+++ src/java/org/xudifsd/zk/ZkClient.java
@@ -11,12 +11,18 @@ public class ZkClient {
 	private final long sessionTimeout = 180000;
 	private CuratorFramework client = null;
 
-	public ZkClient(String connectString) {
-		try {
-			client = CuratorFrameworkFactory.newClient(connectString, new ExponentialBackoffRetry(1000, 3));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public ZkClient(String connectString) throws Exception {
+		client = CuratorFrameworkFactory.newClient(connectString, new ExponentialBackoffRetry(1000, 3));
 		client.start();
+	}
+
+	public ZkClient(String connectString, String namespace) throws Exception {
+		client = CuratorFrameworkFactory.newClient(connectString, new ExponentialBackoffRetry(1000, 3));
+		client.start();
+		client = client.usingNamespace(namespace);
+	}
+
+	public void create(String path, String data) throws Exception {
+		client.create().forPath(path, data.getBytes());
 	}
 }
